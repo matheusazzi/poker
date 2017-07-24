2d78f605a89d03d5bee2a2ef020f1949ca2f5cab
Paulo Diovani Gonçalves <paulo@diovani.com>
2017-07-24 19:27:22 -0300

    Implement pair comparison

--- lib/hand.ex
+++ lib/hand.ex
@@ -1,5 +1,37 @@
 defmodule Hand do
+
   def is_high_card(_) do
     true
+  end
+
+  def is_pair(hand) do
+    hand
+    |> sort_hand
+    |> pair
+  end
+
+  def pair([{_, val}, {_, val}, {_, _}, {_, _}, {_, _}]) do
+    val
+  end
+
+  def pair([{_, _}, {_, val}, {_, val}, {_, _}, {_, _}]) do
+    val
+  end
+
+  def pair([{_, _}, {_, _}, {_, val}, {_, val}, {_, _}]) do
+    val
+  end
+
+  def pair([{_, _}, {_, _}, {_, _}, {_, val}, {_, val}]) do
+    val
+  end
+
+  def pair(_) do
+    false
+  end
+
+  defp sort_hand(hand) do
+    hand
+    |> Enum.sort fn(card1, card2) -> elem(card1, 1) > elem(card2, 1) end
   end
 end
--- test/poker_test.exs
+++ test/poker_test.exs
@@ -4,6 +4,7 @@ defmodule PokerTest do
 
   test "when first hand is higher" do
     assert Poker.winner([{:a, 10}, {:a, 9}], [{:a, 10}, {:a, 8}]) == [{:a, 10}, {:a , 9}]
+    assert Poker.winner([{:a, 14}, {:a, 9}], [{:a, 13}, {:a, 10}]) == [{:a, 14}, {:a, 9}]
   end
 
   test "when second hand is higher" do
@@ -11,6 +12,18 @@ defmodule PokerTest do
   end
 
   test "when higher cards are equal" do
-    assert Poker.winner([{:a, 1}, {:a, 2}, {:h, 12}], [{:a, 1}, {:a, 2}, {:h, 12}]) == :draw
+    assert Poker.winner([{:a, 14}, {:a, 2}, {:h, 12}], [{:a, 14}, {:a, 2}, {:h, 12}]) == :draw
+  end
+
+  test "pair wins against high_card" do
+    pair = [{:a, 2}, {:a, 2}, {:h, 12}, {:h, 10}, {:h, 9}]
+    high_card =  [{:a, 14}, {:a, 2}, {:h, 12}, {:h, 11}, {:h, 10}]
+    assert Poker.winner(pair, high_card) == pair
+  end
+
+  test "greater pair wins" do
+    hand_one = [{:a, 2}, {:a, 2}, {:h, 12}, {:h, 10}, {:h, 9}]
+    hand_two = [{:a, 3}, {:a, 3}, {:h, 7}, {:h, 10}, {:h, 9}]
+    assert Poker.winner(hand_two, hand_one) == hand_two
   end
 end
