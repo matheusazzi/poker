635b0e106fe2362d4b5669cbe60c03383490fdbc
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-08-04 17:45:10 -0300

    fix test warning

--- test/hand_test.exs
+++ test/hand_test.exs
@@ -2,18 +2,8 @@ defmodule HandTest do
   use ExUnit.Case
   doctest Hand
 
-  def first_hand do
-    [
-      {:spade, 5},
-      {:club, 4},
-      {:heart, 3},
-      {:diamond, 6},
-      {:diamond, 11}
-    ]
-  end
-
   test "#is_high_card" do
-    assert Hand.is_high_card(first_hand)
+    assert Hand.is_high_card([{:spade, 5}, {:club, 4}, {:heart, 3}, {:diamond, 6}, {:diamond, 11}])
   end
 
   test "#is_pair" do
